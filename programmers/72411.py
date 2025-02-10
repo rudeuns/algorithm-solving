335f11525dcb09366fcf71865f179eb4ade1d8c7
rudeuns <rudeuns@gmail.com>
2025-02-10 22:12:47 +0900

    🎯 Solved: 메뉴 리뉴얼 - 프로그래머스 (Lv.2)
    🛠 Refactor (2025.02.10)
    - 조합과 카운팅 로직을 라이브러리 사용하여 간단하게 구현

--- programmers/72411.py
+++ programmers/72411.py
@@ -1,56 +1,27 @@
-menu = {}
-
-
-def select(idx, order, n, result):
-    if len(result) == n:
-        res = "".join(sorted(result))
-        if res not in menu:
-            menu[res] = 1
-        else:
-            menu[res] += 1
-        result.pop()
-        return
-
-    for i in range(idx, len(order)):
-        result.append(order[i])
-        select(i + 1, order, n, result)
-
-    if result:
-        result.pop()
+from itertools import combinations
+from collections import Counter
 
 
 def solution(orders, course):
     answer = []
 
-    for order in orders:
-        n = len(order)
-
-        for c in course:
-            if n < c:
-                break
-            select(0, order, c, [])
-
-    max_cnt = [2] * 11
-    max_menu = {}
     for c in course:
-        max_menu[c] = []
+        menu = []
 
-    for name, cnt in menu.items():
-        n = len(name)
-        if cnt > max_cnt[n]:
-            max_cnt[n] = cnt
-            max_menu[n] = [name]
-        elif cnt == max_cnt[n]:
-            max_menu[n].append(name)
+        for order in orders:
+            menu += combinations(sorted(order), c)
 
-    for value in max_menu.values():
-        answer.extend(value)
+        counter = Counter(menu)
+        sorted_counter = counter.most_common()
+
+        max_cnt = 2
+        for menu_tuple, cnt in sorted_counter:
+            if cnt < max_cnt:
+                break
+            else:
+                max_cnt = cnt
+                answer.append("".join(menu_tuple))
 
     answer.sort()
 
     return answer
-
-
-"""
-🎯 Solved (2025.02.10)
-"""
